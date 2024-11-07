fc6e1bad0189ba86a2532fbaefcb838251737291
vizz-g <nikita355hleb@gmail.com>
2024-11-07 18:39:58 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+number_of_homework_completed=12
+number_of_hours_spent=1.5
+course_name="Python"
+time_per_task=number_of_hours_spent/number_of_homework_completed
+print(f"Курс:{course_name}, всего задач:{number_of_homework_completed}, затрачено часов:{number_of_hours_spent}, среднее время выполнения {time_per_task} часа.")
